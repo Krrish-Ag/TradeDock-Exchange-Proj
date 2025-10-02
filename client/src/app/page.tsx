6bfdfed9c7091f2d00cda8f01237cc35b8835cd7
KRRISH AGARWAL <krrishapj@gmail.com>
2025-10-02 14:06:31 +0530

    data in a separate file

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,30 +1,7 @@
 import Link from "next/link";
+import { supportedMarkets } from "./utils/data";
 
 //list of markets
-const supportedMarkets = [
-  { name: "TATA_INR", base: "TATA", quote: "INR", icon: "/icons/tata.png" },
-  {
-    name: "RELIANCE_INR",
-    base: "RELIANCE",
-    quote: "INR",
-    icon: "/icons/reliance.png",
-  },
-  {
-    name: "AMAZON_INR",
-    base: "AMAZON",
-    quote: "INR",
-    icon: "/icons/amazon.png",
-  },
-  { name: "WIPRO_INR", base: "WIPRO", quote: "INR", icon: "/icons/wipro.png" },
-  {
-    name: "GOOGLE_INR",
-    base: "GOOGLE",
-    quote: "INR",
-    icon: "/icons/google.png",
-  },
-  { name: "HDFC_INR", base: "HDFC", quote: "INR", icon: "/icons/hdfc.png" },
-  // ...and so on
-];
 
 export default function Home() {
   return (
---
+++ client/src/app/utils/data.ts
@@ -0,0 +1,24 @@
+export const supportedMarkets = [
+  { name: "TATA_INR", base: "TATA", quote: "INR", icon: "/icons/tata.png" },
+  {
+    name: "RELIANCE_INR",
+    base: "RELIANCE",
+    quote: "INR",
+    icon: "/icons/reliance.png",
+  },
+  {
+    name: "AMAZON_INR",
+    base: "AMAZON",
+    quote: "INR",
+    icon: "/icons/amazon.png",
+  },
+  { name: "WIPRO_INR", base: "WIPRO", quote: "INR", icon: "/icons/wipro.png" },
+  {
+    name: "GOOGLE_INR",
+    base: "GOOGLE",
+    quote: "INR",
+    icon: "/icons/google.png",
+  },
+  { name: "HDFC_INR", base: "HDFC", quote: "INR", icon: "/icons/hdfc.png" },
+  // ...and so on
+];
